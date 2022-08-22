796d305726a782057357d7da0b1efd54acf3644a
rebecca <rebecca.yu@senao.com.tw>
2022-08-22 16:40:02 +0800

    修改datepicker

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,7 +1,7 @@
 const elem = document.querySelector('input[name="datepicker"]');
 if (elem) {
   const datepicker = new Datepicker(elem, {
-    format: "yyyy/mm/dd",
-    language: "zh-TW",
+    autohide: true,
+    language: "zh-CN",
   });
 }
