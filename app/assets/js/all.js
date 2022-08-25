456ef9b185afa40ac0d52221fb32b0ee79a6b555
Rebecca <rebecca.yu@senao.com.tw>
2022-08-25 12:20:40 +0800

    套用swiper, 尚未customize style

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -5,3 +5,15 @@ if (elem) {
     language: "zh-CN",
   });
 }
+
+const swiper = new Swiper('.swiper', {
+  pagination: {
+  el: '.swiper-pagination',
+  clickable: true
+  },
+
+  navigation: {
+  nextEl: '.swiper-button-next',
+  prevEl: '.swiper-button-prev',
+  },
+});
