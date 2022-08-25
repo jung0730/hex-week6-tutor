deef60977bdaafe283591bbc1ca5c096182fc53b
Rebecca <rebecca.yu@senao.com.tw>
2022-08-25 14:15:29 +0800

    Update 2022-08-25T06:15:27.030Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -8,4 +8,15 @@ if (elem) {
     language: "zh-CN"
   });
 }
+
+var swiper = new Swiper('.swiper', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
+});
 //# sourceMappingURL=all.js.map
